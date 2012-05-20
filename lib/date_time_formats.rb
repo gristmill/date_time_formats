1b82033f3126cdc2fc2478f01381897ca0d06e62
Sean Behan <inbox@seanbehan.com>
2012-05-20 12:04:04 -0400

    full path

--- lib/date_time_formats.rb
+++ lib/date_time_formats.rb
@@ -2,7 +2,7 @@ module DateTimeFormats
 
   def self.formats
     rails, default = {}, {}
-    default = YAML.load_file("date_time_formats.yml")
+    default = YAML.load_file("#{File.dirname(__FILE__)}/date_time_formats.yml")
     rails   = YAML.load_file("#{Rails.root}/config/date_time_formats.yml") if defined?(Rails) && File.exists?("#{Rails.root}/config/date_time_formats.yml")
 
     default.merge(rails) # Rails.root/config/date_time_formats.yml override defaults
